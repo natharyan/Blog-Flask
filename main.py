8f156041e0df997503c25d15f12b284c14601e64
Aryan Nath <aryannath2004@gmail.com>
2023-01-17 02:19:37 +0530

    ⚙🔨added config.json

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import json
 from datetime import datetime
 import pymysql
 pymysql.install_as_MySQLdb()
 
+local_server = True
+with open('config.json','r') as c:
+    params = json.load(c)["params"]
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/blog_flask"
+if local_server:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['local_uri']
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['prod_uri']
 db.init_app(app)
 
 
@@ -21,11 +28,11 @@ class Contacts(db.Model):
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', params = params)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params = params)
 
 @app.route("/contact", methods = ['GET','POST'])
 def contact():
@@ -37,10 +44,10 @@ def contact():
         entry = Contacts(name = name, phone_num = phone, msg = message, date = datetime.now(), email = email)
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html')
+    return render_template('contact.html', params = params)
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html', params = params)
 
 app.run(debug=True)
