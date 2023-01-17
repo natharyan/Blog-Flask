6e34ca9b28caeec7392d1c0f34f33e4be2e659c5
Aryan Nath <aryannath2004@gmail.com>
2023-01-18 00:26:15 +0530

    📄 added pagination

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_mail import Mail
 import json
 import os
 from datetime import datetime
+import math
 import pymysql
 pymysql.install_as_MySQLdb()
 
@@ -50,8 +51,27 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().limit(params['no_of_posts']).all()
-    return render_template('index.html', params = params, posts = posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+
+    page = request.args.get('page')
+    if not str(page).isnumeric():
+        # page is not numeric - returned by page url
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']): (page-1)*int(params['no_of_posts']) + int(params['no_of_posts'])]
+    if page == 1:
+        prev = "#"
+        next = "/?page=" + str(page + 1)
+    elif page == last:
+        # next => page = 1 so prev remains the same
+        prev = "/?page=" + str(page - 1)
+        next = "#"
+    else:
+        prev = "/?page=" + str(page - 1)
+        next = "/?page=" + str(page + 1)
+    
+    return render_template('index.html', params = params, posts = posts, prev = prev, next = next)
 
 @app.route("/about")
 def about():
